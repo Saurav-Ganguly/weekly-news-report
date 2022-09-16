5adee6d64785d3b4e6d32f454042591df2cc12c8
Saurav Ganguly <113334601+Saurav-Ganguly@users.noreply.github.com>
2022-09-16 06:52:56 +0000

    finished

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
-# REST Api -> Reprasentational state transfer Application programming 
+# weekly news report
 import requests
+import datetime as DT
 
 api_key = os.environ['api_key']
 
-# print(api_key)
-url = f"https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-9-10&to=2022-9-15&sortBy=popularity&language=en&apiKey={api_key}"
+topic = input("What topic of news?: ")
+# if topic containes spaces add %20
+topic_array = topic.split(" ")
+topic_str = "%20".join(topic_array)
+date_today = DT.date.today()
+date_today_str = date_today.strftime("%Y-%-m-%-d")
+date_week_ago = date_today - DT.timedelta(days=7)
+date_week_ago_str = date_week_ago.strftime("%Y-%-m-%-d")
+
+url = f"https://newsapi.org/v2/everything?qInTitle={topic_str}&from={date_week_ago_str}&to={date_today_str}&sortBy=popularity&language=en&apiKey={api_key}"
 r = requests.get(url)
 content = r.json()
 articles = content["articles"]
 for item in articles:
-  print(item["title"])
-  print("*---------------------------------*")
+    print(item["title"])
+    print("*---------------------------------*")
